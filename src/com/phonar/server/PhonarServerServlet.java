b58374608b9a4857139223e4986730490531c255
Jeffrey Hodes <jeffreyhodes@gmail.com>
2011-07-30 13:26:35 -0700

    changed package name

---
+++ src/com/phonar/server/PhonarServerServlet.java
@@ -0,0 +1,19 @@
+package com.phonar.server;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+import com.location.phonar.common.CommonApi;
+
+@SuppressWarnings("serial")
+public class PhonarServerServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		
+		String number = req.getParameter(CommonApi.MY_NUMBER_PARAM);
+		String target = req.getParameter(CommonApi.TARGET_NUMBER_PARAM);
+		
+		resp.setContentType("text/plain");
+		resp.getWriter().println(number + " wants to know where " + target + " is.");
+	}
+}
