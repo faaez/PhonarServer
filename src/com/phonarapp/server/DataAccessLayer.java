235a266f7d9f154bf5acdd128f2496fa0aebfff5
Jeffrey Hodes <jeffreyhodes@gmail.com>
2011-07-30 20:49:12 -0700

    Removed cruft

--- src/com/phonarapp/server/DataAccessLayer.java
+++ src/com/phonarapp/server/DataAccessLayer.java
@@ -1,16 +1,56 @@
 package com.phonarapp.server;
 
+import java.util.List;
+
+import org.apache.tools.ant.types.resources.Tokens;
+
+import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Query;
 
 public class DataAccessLayer {
+	
+	public static final String AUTH_TOKEN_STRING = "DQAAAMMAAACL7u3MM621pp26GLzkRT9ELIXGxqPBOPRZrWezKkujGzKqit2ZkSun8jXxMVpUe4g2MDGT18RGuFaSFLIcdLkznm1Lw05tiDD7trm9dsNgOA7sPIsejdRiNv1EcZ_ZXKug5CuNYkQN0EbjtWomDyi-xBLWAYZo_8LKxaBm3xfLyMEYx3T2r41NlrnPQU5Wnc47_Q0O0IFkKhboayg6vNHKlVFtYJOIDcJblknuD0mL8lvQjCBQmKeop80LZuotkKEEKesjziOiPYphNVsj-cMg";
+	
 	public static void registerUser(String number, String registrationId) {
 		Key userKey = KeyFactory.createKey("User", number);
 		Entity entity = new Entity(userKey);
 		entity.setProperty("number", number);
 		entity.setProperty("registrationId", registrationId);
 		DatastoreServiceFactory.getDatastoreService().put(entity);
+	}
+	
+	public static String getUserRegId(String number) {
+		Key userKey = KeyFactory.createKey("User", number);
+		Query query = new Query("User", userKey);
+		DatastoreService datastore 
+			= DatastoreServiceFactory.getDatastoreService();
+		List<Entity> users = datastore.prepare(query).asList(
+				FetchOptions.Builder.withLimit(1));
+		Entity user = users.get(0);
+		return (String) user.getProperty("registrationId");
+	}
+	
+	public static void storeAuthToken(String token) {
+		Key tokenKey = KeyFactory.createKey("AuthToken", "AuthToken");
+		Entity entity = new Entity(tokenKey);
+		entity.setProperty("token", token);
+		DatastoreServiceFactory.getDatastoreService().put(entity);
+	}
+	
+	public static String getAuthToken() {
+
+		Key tokenKey = KeyFactory.createKey("AuthToken", "AuthToken");
+	    Query query = new Query("AuthToken", tokenKey);
+	    DatastoreService datastore 
+	    	= DatastoreServiceFactory.getDatastoreService();
+	    List<Entity> tokens = datastore.prepare(query).asList(
+	    		FetchOptions.Builder.withLimit(1));
+	    //Entity token = tokens.get(0);
+		return AUTH_TOKEN_STRING;
 	}
 }
--- src/com/phonarapp/server/PhonarServerServlet.java
+++ src/com/phonarapp/server/PhonarServerServlet.java
@@ -1,7 +1,11 @@
 package com.phonarapp.server;
 
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.phonarapp.common.CommonApi;
 
@@ -10,10 +14,14 @@ public class PhonarServerServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		
-		String number = req.getParameter(CommonApi.MY_NUMBER_PARAM);
+		//String number = req.getParameter(CommonApi.MY_NUMBER_PARAM);
 		String target = req.getParameter(CommonApi.TARGET_NUMBER_PARAM);
 		
-		resp.setContentType("text/plain");
-		resp.getWriter().println(number + " wants to know where " + target + " is.");
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("data.lol", "yoyojorge");
+		
+		C2DMPusher.sendNoRetry(DataAccessLayer.getUserRegId(target),
+				"something", map, false );
+		
 	}
 }
