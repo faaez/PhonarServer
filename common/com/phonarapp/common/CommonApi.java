1f53e1c35296ae93c0654d614687fe16d65f9b07
Jeffrey Hodes <jeffreyhodes@gmail.com>
2011-07-30 20:50:16 -0700

    Added a comment

--- common/com/phonarapp/common/CommonApi.java
+++ common/com/phonarapp/common/CommonApi.java
@@ -1,6 +1,7 @@
 package com.phonarapp.common;
 
 public class CommonApi {
+	// This is a comment, from Jeff to Jorge.
 	public static String MY_NUMBER_PARAM = "number";
 	public static String TARGET_NUMBER_PARAM = "target";
 	public static String REGISTRATION_ID_PARAM = "registrationId";
